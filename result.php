0fd05285ca26c82f68be92f347367c2eba827643
mattia0419 <pironemattia6@gmail.com>
2023-09-25 17:28:32 +0200

    fix html

--- result.php
+++ result.php
@@ -16,13 +16,17 @@ $paragrafo_censurato = str_replace($parola_da_censurare, "***", $paragraph);
 </head>
 <body>
     <p> 
-        <h1>Paragrafo</h1>
+        <h1>PARAGRAFO:</h1>
+        <?php echo $paragraph;?>
         
-        <?php
-         echo $paragraph;
-         
-         ?>
-        lunghezza:<?php echo $lunghezza;?>
+         <h1>
+            NUMERO DI PAROLE:
+         </h1>
+         <?php echo $lunghezza;?>
+        
+         <h1>
+            PARAGRAFO CENSURATO:
+         </h1>
         <?php echo $paragrafo_censurato;?>
 
     </p>
